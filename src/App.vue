d6e8c48bea30d1347a2ea076c21ae34f59e84341
Sean Xiao <sean.xiao@designcrowd.com>
2024-11-14 12:20:06 +1100

    lhs

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,33 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 
-// Available DaisyUI themes
 const themes = ['light', 'dark', 'black', 'luxury', 'cyberpunk']
 
-const currentTheme = ref(localStorage.getItem('theme') || 'light')
+const currentTheme = ref('light')
+const showSuccessAlert = ref(false)
+const successAlertContent = ref('')
+const showInfoAlert = ref(false)
+const infoAlertContent = ref('')
+
+const toggleSuccessAlert = (content: string) => {
+  showSuccessAlert.value = true
+  successAlertContent.value = content
+  setTimeout(() => {
+    showSuccessAlert.value = false
+    successAlertContent.value = ''
+  }, 1000)
+}
+
+const toggleInfoAlert = (content: string) => {
+  showInfoAlert.value = true
+  infoAlertContent.value = content
+  setTimeout(() => {
+    showInfoAlert.value = false
+    infoAlertContent.value = ''
+  }, 1000)
+}
 
 const updateTheme = (theme: string) => {
-  console.log(theme)
   currentTheme.value = theme
   localStorage.setItem('theme', theme)
 }
@@ -22,7 +42,7 @@ if (typeof window !== 'undefined') {
 </script>
 
 <template>
-  <div class="min-h-screen" :data-theme="currentTheme">
+  <div class="h-screen" :data-theme="currentTheme">
     <nav class="navbar bg-base-300">
       <div class="flex-1">
         <div class="breadcrumbs text-lg">
@@ -50,7 +70,70 @@ if (typeof window !== 'undefined') {
       </div>
     </nav>
 
-    <main class="container mx-auto p-4"></main>
+    <div id="alerts" class="absolute top-22 w-full">
+      <div v-if="showSuccessAlert" role="alert" class="alert alert-success">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-6 w-6 shrink-0 stroke-current"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+          />
+        </svg>
+        <span>{{ successAlertContent }}</span>
+      </div>
+
+      <div v-if="showInfoAlert" role="alert" class="alert alert-info">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-6 w-6 shrink-0 stroke-current"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+          />
+        </svg>
+        <span>{{ infoAlertContent }}</span>
+      </div>
+    </div>
+
+    <main class="p-4 w-full flex gap-4">
+      <div class="left-side-container flex flex-col flex-grow gap-4 items-center">
+        <div id="maker-canvas" class="hero border-4 border-dashed" style="height: 600px">
+          <div class="hero-content text-center">
+            <h1 class="text-5xl font-bold">this is the canvas</h1>
+          </div>
+        </div>
+
+        <div id="maker-bottom-container" class="rounded-box bg-base-200 absolute bottom-4">
+          <div class="hero-content text-center">
+            <button class="btn btn-primary w-36" @click="toggleSuccessAlert('Your logo is saved!')">
+              Save progress
+            </button>
+            <button
+              class="btn btn-secondary w-36"
+              @click="toggleInfoAlert('Your logo is downloading...')"
+            >
+              Download
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div class="right-side-container flex-grow">
+        <div id="tabs" class="flex flex-col"></div>
+        <div class="rounded-box bg-base-200 p-4">Sidebar</div>
+      </div>
+    </main>
   </div>
 </template>
 
